d2c7bdf38de8ddda4ca3bc00fd89a6124e169d88
bcadev2gmail <bcadev2@gmail.com>
2018-02-15 16:29:31 +0700

    Update config.php

--- config.php
+++ config.php
@@ -22,9 +22,9 @@
 // 	$dbtype = "MySQLi";
 
 	//PostGre
-	require_once ("../codebase/connector/db_postgre.php");
-	$res = pg_connect("dbname=dae7tev5vslgku host=ec2-54-235-90-200.compute-1.amazonaws.com port=5432 user=eqrjpfbvzghnmz password=8c0160dafdaa082eaf2fc85a18cd76ca7c943530db9c9aa5769fd8cb36ab651e sslmode=require");
-	$dbtype = "Postgre";
+// 	require_once ("../codebase/connector/db_postgre.php");
+// 	$res = pg_connect("dbname=dae7tev5vslgku host=ec2-54-235-90-200.compute-1.amazonaws.com port=5432 user=eqrjpfbvzghnmz password=8c0160dafdaa082eaf2fc85a18cd76ca7c943530db9c9aa5769fd8cb36ab651e sslmode=require");
+// 	$dbtype = "Postgre";
 
 	$dbopts = parse_url(getenv('postgres://eqrjpfbvzghnmz:8c0160dafdaa082eaf2fc85a18cd76ca7c943530db9c9aa5769fd8cb36ab651e@ec2-54-235-90-200.compute-1.amazonaws.com:5432/dae7tev5vslgku'));
 	$app->register(new Herrera\Pdo\PdoServiceProvider(),
