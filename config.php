cd62682b2bc8ac4fc5b67d19601fed85312158f1
bcadev2gmail <bcadev2@gmail.com>
2018-02-15 18:12:04 +0700

    Update config.php

--- config.php
+++ config.php
@@ -22,16 +22,16 @@
 // 	$dbtype = "MySQLi";
 
 	//PostGre
-// 	require_once ("../codebase/connector/db_postgre.php");
-// 	$res = pg_connect("dbname=dae7tev5vslgku host=ec2-54-235-90-200.compute-1.amazonaws.com port=5432 user=eqrjpfbvzghnmz password=8c0160dafdaa082eaf2fc85a18cd76ca7c943530db9c9aa5769fd8cb36ab651e sslmode=require");
-// 	$dbtype = "Postgre";
-	
-	function pg_connection_string_from_database_url() {
-	  extract(parse_url($_ENV["DATABASE_URL"]));
-	  return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
-	}
-	# Here we establish the connection. Yes, that's all.
-	$res = pg_connect(pg_connection_string_from_database_url());
+	require_once ("../codebase/connector/db_postgre.php");
+	$res = pg_connect("dbname=dae7tev5vslgku host=ec2-54-235-90-200.compute-1.amazonaws.com port=5432 user=eqrjpfbvzghnmz password=8c0160dafdaa082eaf2fc85a18cd76ca7c943530db9c9aa5769fd8cb36ab651e sslmode=require");
 	$dbtype = "Postgre";
+	
+// 	function pg_connection_string_from_database_url() {
+// 	  extract(parse_url($_ENV["DATABASE_URL"]));
+// 	  return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
+// 	}
+// 	# Here we establish the connection. Yes, that's all.
+// 	$res = pg_connect(pg_connection_string_from_database_url());
+// 	$dbtype = "Postgre";
 	
 ?>
