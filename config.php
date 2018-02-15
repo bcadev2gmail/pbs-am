16e66518774df468dd8d04c98b02e121ceceec83
bcadev2gmail <bcadev2@gmail.com>
2018-02-15 15:28:37 +0700

    Update config.php

--- config.php
+++ config.php
@@ -17,7 +17,12 @@
 	// $dbtype = "PDO";
 
 	// MySQLi
-	require_once ("../codebase/connector/db_mysqli.php");
-	$res = mysqli_connect("localhost","m26415039","UKP889","m26415039");
-	$dbtype = "MySQLi";
+// 	require_once ("../codebase/connector/db_mysqli.php");
+// 	$res = mysqli_connect("localhost","m26415039","UKP889","m26415039");
+// 	$dbtype = "MySQLi";
+
+	//PostGre
+	require_once ("../codebase/connector/db_postgre.php");
+	$res = pg_connect("dbname=dae7tev5vslgku host=ec2-54-235-90-200.compute-1.amazonaws.com port=5432 user=eqrjpfbvzghnmz password=8c0160dafdaa082eaf2fc85a18cd76ca7c943530db9c9aa5769fd8cb36ab651e sslmode=require");
+	$dbtype = "Postgre";
 ?>
