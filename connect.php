3f6bf0ebe9e2d8a85f721b1a14fbc2b118726757
bcadev2gmail <bcadev2@gmail.com>
2018-02-15 15:22:28 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,3 +1,4 @@
 <?php
-    $con = mysqli_connect("localhost","m26415039","UKP889","m26415039");
+  #  $con = mysqli_connect("localhost","m26415039","UKP889","m26415039");
+    $con = pg_connect("dbname=dae7tev5vslgku host=ec2-54-235-90-200.compute-1.amazonaws.com port=5432 user=eqrjpfbvzghnmz password=8c0160dafdaa082eaf2fc85a18cd76ca7c943530db9c9aa5769fd8cb36ab651e sslmode=require");
 ?>
