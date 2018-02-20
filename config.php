7093b98bdebcd4aa0c2278438e2bc19e68ed7b73
bcadev2gmail <bcadev2@gmail.com>
2018-02-20 09:49:11 +0700

    Update config.php

--- config.php
+++ config.php
@@ -12,10 +12,10 @@
 	// mysql_select_db("schedulertest");
 
 	// PDO
-	require_once ('../codebase/connector/db_pdo.php");
-	$dsn = "pgsql:"."host=ec2-184-73-194-179.compute-1.amazonaws.com;"."dbname=ul28zxpr39no1rr;"."user=dj1wcxb3x9fy3x5;"."port=5432;"."sslmode=require;"."password=p28xwd9pjcrzyzp6mf74m99cze";
-	$res = new PDO("pgsql:dbname=dae7tev5vslgku host=ec2-54-235-90-200.compute-1.amazonaws.com port=5432 user=eqrjpfbvzghnmz password=8c0160dafdaa082eaf2fc85a18cd76ca7c943530db9c9aa5769fd8cb36ab651e sslmode=require");
-	$dbtype = "PDO";
+// 	require_once ('../codebase/connector/db_pdo.php");
+// 	$dsn = "pgsql:"."host=ec2-184-73-194-179.compute-1.amazonaws.com;"."dbname=ul28zxpr39no1rr;"."user=dj1wcxb3x9fy3x5;"."port=5432;"."sslmode=require;"."password=p28xwd9pjcrzyzp6mf74m99cze";
+// 	$res = new PDO("pgsql:dbname=dae7tev5vslgku host=ec2-54-235-90-200.compute-1.amazonaws.com port=5432 user=eqrjpfbvzghnmz password=8c0160dafdaa082eaf2fc85a18cd76ca7c943530db9c9aa5769fd8cb36ab651e sslmode=require");
+// 	$dbtype = "PDO";
 
 	// MySQLi
 // 	require_once ("../codebase/connector/db_mysqli.php");
@@ -23,9 +23,9 @@
 // 	$dbtype = "MySQLi";
 
 	//PostGre
-// 	require_once ("../codebase/connector/db_postgre.php");
-// 	$res = pg_connect("dbname=dae7tev5vslgku host=ec2-54-235-90-200.compute-1.amazonaws.com port=5432 user=eqrjpfbvzghnmz password=8c0160dafdaa082eaf2fc85a18cd76ca7c943530db9c9aa5769fd8cb36ab651e sslmode=require");
-// 	$dbtype = "Postgre";
+	require_once ("../codebase/connector/db_postgre.php");
+	$res = pg_connect("dbname=dae7tev5vslgku host=ec2-54-235-90-200.compute-1.amazonaws.com port=5432 user=eqrjpfbvzghnmz password=8c0160dafdaa082eaf2fc85a18cd76ca7c943530db9c9aa5769fd8cb36ab651e sslmode=require");
+	$dbtype = "Postgre";
 	
 //  	function pg_connection_string_from_database_url() {
 //  	  extract(parse_url($_ENV["DATABASE_URL"]));
